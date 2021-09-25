3b2ea6a1dd646e97b742ddf27829be30c45a229e
ianhudson87 <ihudson@wustl.edu>
2021-09-24 22:42:09 -0500

    fixed uniform columsn mask

--- sampleMask.py
+++ sampleMask.py
@@ -3,16 +3,15 @@ import random
 
 
 def get_sample_mask(h, w, p):
-    # take columns at random, when a column is chosen, the mirrored column is also automatically chosen
+    # take columns at random
     # h=height, w=width, p=percent of columns chosen
-    num_mask_columns = int(w/2*p) # number of columns to choose (not including mirror columns)
+    num_mask_columns = int(w*p) # number of columns to choose
     # print(num_mask_columns)
     mask = torch.zeros(h, w)
-    columns_chosen = random.sample(range(int((w+1)/2)), num_mask_columns) # columns from left side of image, need to also take the mirror column
+    columns_chosen = random.sample(range(w, num_mask_columns) # columns from left side of image, need to also take the mirror column
     # print(columns_chosen)
     for column in columns_chosen:
         mask[:, column] = 1
-        mask[:, (w-1)-column] = 1
 
     return mask
     # print(mask)
